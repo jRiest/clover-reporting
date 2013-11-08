359800df0eb3d7c8c04e42fcd2b9e4331cb5e6ea
Jake Riesterer <jake.riesterer@mutualmobile.com>
2013-11-08 16:02:28 -0600

    Cleaning out gh-pages branch

--- src/www/js/app/cache/localStore.js
+++
@@ -1,5 +0,0 @@
-define(function(require) {
-	var LocalStore = require('lavaca/storage/LocalStore');
-  var localStore = new LocalStore('clover'); // change 'lavaca_app' to your namespace
-	return localStore;
-});
